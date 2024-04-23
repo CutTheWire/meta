3a8ec42dcf37c81bcaf61733d41a8fdafa45bcb9
SeoJungHun <102637600+CaDiVae@users.noreply.github.com>
2024-04-23 14:08:56 +0900

    composer.json, composer.lock add

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/meta',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '49b0dfa448545b97eda085831e8d6852a98a1ff5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/meta' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '49b0dfa448545b97eda085831e8d6852a98a1ff5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
